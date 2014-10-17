21e8e3f17125d0284be40efbb46adeacd6e03d4e
Unknwon <joe2010xtmf@163.com>
2014-10-17 19:07:53 -0400

    Init commit

---
+++ host_switcher.go
@@ -0,0 +1,89 @@
+// Copyright 2014 Unknwon
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package switcher
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/Unknwon/com"
+	"github.com/Unknwon/macaron"
+)
+
+// HostSwitcher represents a global multi-site support layer.
+type HostSwitcher struct {
+	switches map[string]*macaron.Macaron
+	list     []string
+}
+
+// NewHostSwitcher initalizes and returns a new host switcher.
+// You have to use this function to get a new host switcher.
+func NewHostSwitcher() *HostSwitcher {
+	return &HostSwitcher{
+		switches: make(map[string]*macaron.Macaron),
+	}
+}
+
+// Set adds a new switch to host switcher.
+func (hs *HostSwitcher) Set(host string, m *macaron.Macaron) {
+	hs.switches[host] = m
+	hs.list = append(hs.list, host)
+}
+
+// Remove removes a switch from host switcher.
+func (hs *HostSwitcher) Remove(host string) {
+	delete(hs.switches, host)
+	idx := -1
+	for i, name := range hs.list {
+		if name == host {
+			idx = i
+			break
+		}
+	}
+	if idx >= 0 {
+		hs.list = append(hs.list[:idx], hs.list[idx+1:]...)
+	}
+}
+
+// ServeHTTP is the HTTP Entry point for a Host Switcher instance.
+func (hs *HostSwitcher) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	if h := hs.switches[req.Host]; h != nil {
+		h.ServeHTTP(resp, req)
+	} else {
+		http.Error(resp, "Not Found", http.StatusNotFound)
+	}
+}
+
+// RunOnAddr runs server in given address and port.
+func (hs *HostSwitcher) RunOnAddr(addr string) {
+	if macaron.Env == macaron.DEV {
+		infos := strings.Split(addr, ":")
+		port := com.StrTo(infos[1]).MustInt()
+		for _, host := range hs.list[:len(hs.list)-1] {
+			go hs.switches[host].RunOnAddr(infos[0] + ":" + com.ToStr(port))
+			port++
+		}
+		hs.switches[hs.list[len(hs.list)-1]].RunOnAddr(infos[0] + ":" + com.ToStr(port))
+		return
+	}
+
+	log.Fatalln(http.ListenAndServe(addr, hs))
+}
+
+// Run the http server. Listening on os.GetEnv("PORT") or 4000 by default.
+func (hs *HostSwitcher) Run() {
+	hs.RunOnAddr(macaron.GetDefaultListenAddr())
+}
